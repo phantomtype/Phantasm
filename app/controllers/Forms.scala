a806948f3b9d4d579317f6401a37365d83fc9bb4
YusukeKokubo <yusuke.kokubo@gmail.com>
2014-01-01 11:18:03 +0900

    clean up

--- app/controllers/Forms.scala
+++
@@ -1,5 +0,0 @@
-package controllers
-
-case class PhotoForm(title: Option[String], description: Option[String])
-case class CommentForm(id: Option[Long], photoId: Long, comment: String)
-
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,19 +4,5 @@
 $(function () {
     'use strict';
 
-    $('#fileupload').fileupload();
-
-    // Load existing files:
-    $('#fileupload').addClass('fileupload-processing');
-    $.ajax({
-        url: $('#fileupload').fileupload('option', 'url'),
-        dataType: 'json',
-        context: $('#fileupload')[0]
-    }).always(function () {
-        $(this).removeClass('fileupload-processing');
-    }).done(function (result) {
-        $(this).fileupload('option', 'done')
-            .call(this, $.Event('done'), {result: result});
-    });
 });
 
