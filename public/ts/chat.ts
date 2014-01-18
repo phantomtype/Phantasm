62ef4b058c5e988cdb68a46eb62539f704f0a2fe
YusukeKokubo <yusuke.kokubo@gmail.com>
2014-01-19 02:12:37 +0900

    ts programming

--- public/ts/chat.ts
+++ public/ts/chat.ts
@@ -1,15 +1,15 @@
 /// <reference path="../d.ts/DefinitelyTyped/jquery/jquery.d.ts" />
 
-var chat_start = function(chatSocket, userId) {
+var chat_start = (chatSocket: WebSocket, userId: number) => {
 
-    var sendMessage = function() {
+    var sendMessage = () => {
         chatSocket.send(JSON.stringify(
             {text: $("#talk").val()}
         ))
         $("#talk").val('')
     }
 
-    var receiveEvent = function(event) {
+    var receiveEvent = (event) => {
         var data = JSON.parse(event.data)
 
         // Handle errors
@@ -24,7 +24,7 @@ var chat_start = function(chatSocket, userId) {
 
         // Create the message element
         var el = $('<div class="message"><img class="avatar" /><span></span><p></p></div>')
-        var user = data.members.filter(function(member) {
+        var user = data.members.filter((member) => {
             return member.id == data.user
         })[0];
         $("img", el).attr("src", user.avatar)
@@ -36,15 +36,15 @@ var chat_start = function(chatSocket, userId) {
 
         // Update the members list
         $("#members").html('')
-        $(data.members).each(function() {
+        data.members.forEach((member) => {
             var li = $('<li><img class="avatar" /><span></span></li>');
-            $("img", li).attr("src", this.avatar)
-            $("span", li).text(this.name)
+            $("img", li).attr("src", member.avatar)
+            $("span", li).text(member.name)
             $("#members").append(li);
         })
     }
 
-    var handleReturnKey = function(e) {
+    var handleReturnKey = (e) => {
         if(e.charCode == 13 || e.keyCode == 13) {
             e.preventDefault()
             sendMessage()
