9f216a308a1aa6d07e7c6d6cf34b2718150cfadf
YusukeKokubo <yusuke.kokubo@gmail.com>
2014-04-01 01:04:56 +0900

    ui improve

--- public/config.rb
+++ public/config.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 # ************************************
 # HTTP Path
 # ************************************
@@ -44,24 +46,24 @@ relative_assets = true
 # CSSファイルにSassファイルの何行目に記述されたものかを出力する
 line_comments = false
 
-# ************************************
-# Sprites
-# ************************************
-# Make a copy of sprites with a name that has no uniqueness of the hash.
-on_sprite_saved do |filename|
-  if File.exists?(filename)
-    FileUtils.cp filename, filename.gsub(%r{-s[a-z0-9]{10}\.png$}, '.png')
-    FileUtils.rm_rf(filename)
-  end
-end
-
-# Replace in stylesheets generated references to sprites
-# by their counterparts without the hash uniqueness.
-on_stylesheet_saved do |filename|
-  if File.exists?(filename)
-    css = File.read filename
-    File.open(filename, 'w+') do |f|
-      f << css.gsub(%r{-s[a-z0-9]{10}\.png}, '.png')
-    end
-  end
-end
+# # ************************************
+# # Sprites
+# # ************************************
+# # Make a copy of sprites with a name that has no uniqueness of the hash.
+# on_sprite_saved do |filename|
+#   if File.exists?(filename)
+#     FileUtils.cp filename, filename.gsub(%r{-s[a-z0-9]{10}\.png$}, '.png')
+#     FileUtils.rm_rf(filename)
+#   end
+# end
+#
+# # Replace in stylesheets generated references to sprites
+# # by their counterparts without the hash uniqueness.
+# on_stylesheet_saved do |filename|
+#   if File.exists?(filename)
+#     css = File.read filename
+#     File.open(filename, 'w+') do |f|
+#       f << css.gsub(%r{-s[a-z0-9]{10}\.png}, '.png')
+#     end
+#   end
+# end
